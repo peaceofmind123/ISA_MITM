0d1871426ee8ef1bab49bc5154c1a389ed069b02
ashishthesatan <ashishthesatan@gmail.com>
2021-06-01 17:50:17 +0545

    created skeleton for user side

---
+++ User/user/components/DefaultCard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Button, Card, Typography} from "antd";
+const {Text} = Typography;
+
+export default function DefaultCard({title, buttonOnClick, text=''}) {
+    return
+}
--- User/user/pages/index.js
+++ User/user/pages/index.js
@@ -1,10 +1,46 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {Button, Card, Typography, Input} from "antd";
+import DefaultCard from "../components/DefaultCard";
+const {Text} = Typography;
 
 export default function Home() {
   return (
-      <div></div>
+      <div style={{height:"100vh", padding:"0 2rem", display: "flex", flexDirection:"column", alignItems:'flex-start', justifyContent:"space-around" }}>
+          <Card title={'Get server\'s address'}>
+              <div style={{minWidth:"40rem", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
+                  <Button type={"primary"}>Get</Button>
+                  <Text></Text>
+              </div>
+
+          </Card>
+          <Card title={'Generate Public/Private key pair'}>
+              <div style={{minWidth:"40rem", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
+                  <Button type={"primary"}>Generate</Button>
+                  <Text></Text>
+              </div>
+
+          </Card>
+          <Card title={'Send public key fetch request to server'}>
+              <div style={{minWidth:"40rem", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
+                  <Button type={"primary"}>Send</Button>
+                  <Text></Text>
+              </div>
+
+          </Card>
+          <Card title={'Encrypt and send message'}>
+              <div>
+                  <Input placeholder={'message'} onChange={e => {}}/>
+              </div>
+              <div style={{minWidth:"40rem", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
+                  <Button type={"primary"}>Send</Button>
+                  <Text></Text>
+              </div>
+
+          </Card>
+
+      </div>
 
   )
 }
