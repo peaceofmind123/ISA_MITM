defe6aa11d2d86bc8e30d7584b4ef4bf3f2c35e0
ashishthesatan <ashishthesatan@gmail.com>
2021-06-03 08:00:47 +0545

    encryption and message passing left

--- attacker/pages/index.js
+++ attacker/pages/index.js
@@ -2,15 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {Button, Card, Typography, Input} from "antd";
+
 const {Text, Paragraph} = Typography;
 import React from 'react';
 
 export default function Home() {
     const [publicKey, setPublicKey] = React.useState('');
     const [privateKey, setPrivateKey] = React.useState('');
+    const [serverPublicKey, setServerPublicKey] = React.useState('');
 
     const handleGenerateKeys = async () => {
-        const response = await fetch('http://localhost:5500/api/generateKeys', {mode:'cors'}).catch(console.log)
+        const response = await fetch('http://localhost:5500/api/generateKeys', {mode: 'cors'}).catch(console.log)
         const data = await response.json().catch(console.log);
         console.log(data);
         setPublicKey(data?.publicKey ?? '');
@@ -20,54 +22,106 @@ export default function Home() {
     };
 
     const handleGetServerPublicKey = async () => {
-        const response = await fetch('http://localhost:5500/api/getServerKey', {mode:'cors'}).catch(console.log)
+        const response = await fetch('http://localhost:5500/api/getServerKey', {mode: 'cors'}).catch(console.log)
         const data = await response.json().catch(console.log);
-        console.log(data);
+        setServerPublicKey(data.publicKey)
+        localStorage.setItem('serverPublicKey', data?.publicKey ?? '')
 
     };
     React.useEffect(() => {
-        setPublicKey(localStorage.getItem('publicKey')??'');
-        setPrivateKey(localStorage.getItem('privateKey')??'');
+        setPublicKey(localStorage.getItem('publicKey') ?? '');
+        setPrivateKey(localStorage.getItem('privateKey') ?? '');
+        setServerPublicKey(localStorage.getItem('serverPublicKey') ?? '');
 
     }, []);
-  return (
-    <>
-        <div style={{height:"100vh", padding:"0 2rem", display: "flex", flexDirection:"column", alignItems:'flex-start', justifyContent:"space-around" }}>
-            <Card title={'Public/private key pair'}>
-                <div style={{minWidth:"40rem", marginBottom:"2rem", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
-                    <Button type={'primary'} onClick={handleGenerateKeys}>Generate</Button>
-                </div>
+    return (
+        <>
+            <div style={{
+                height: "100vh",
+                padding: "0 2rem",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: 'flex-start',
+                justifyContent: "space-around"
+            }}>
+                <Card title={'Public/private key pair'}>
+                    <div style={{
+                        minWidth: "40rem",
+                        marginBottom: "2rem",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between"
+                    }}>
+                        <Button type={'primary'} onClick={handleGenerateKeys}>Generate</Button>
+                    </div>
 
-                <div style={{minWidth:"40rem", marginBottom:"2rem", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
-                    <Text>Public Key</Text>
-                    <Paragraph ellipsis={{ rows: 2, expandable: true, symbol: 'more' }}>{publicKey}</Paragraph>
-                </div>
-                <div style={{minWidth:"40rem", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
-                    <Text>Private Key</Text>
-                    <Paragraph ellipsis={{ rows: 2, expandable: true, symbol: 'more' }}>{privateKey}</Paragraph>
+                    <div style={{
+                        minWidth: "40rem",
+                        marginBottom: "2rem",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between"
+                    }}>
+                        <Text>Public Key</Text>
+                        <Paragraph ellipsis={{rows: 2, expandable: true, symbol: 'more'}}>{publicKey}</Paragraph>
+                    </div>
+                    <div style={{
+                        minWidth: "40rem",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between"
+                    }}>
+                        <Text>Private Key</Text>
+                        <Paragraph ellipsis={{rows: 2, expandable: true, symbol: 'more'}}>{privateKey}</Paragraph>
 
-                </div>
-            </Card>
-            <Card title={'Server\'s public key'}>
-                <div style={{minWidth:"40rem", marginBottom:"2rem", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
-                    <Button type={'primary'} onClick={handleGetServerPublicKey}>Get</Button>
-                </div>
-                <div style={{minWidth:"40rem", marginBottom:"2rem", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
-                    <Text>Public Key</Text>
-                    <Text></Text>
-                </div>
-            </Card>
-            <Card title={'Messages'}>
-                <div style={{minWidth:"40rem", marginBottom:"2rem", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
-                    <Text>Encrypted message</Text>
-                    <Text></Text>
-                </div>
-                <div style={{minWidth:"40rem", marginBottom:"2rem", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
-                    <Text>Decrypted message</Text>
-                    <Text></Text>
-                </div>
-            </Card>
-        </div>
+                    </div>
+                </Card>
+                <Card title={'Server\'s public key'}>
+                    <div style={{
+                        minWidth: "40rem",
+                        marginBottom: "2rem",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between"
+                    }}>
+                        <Button type={'primary'} onClick={handleGetServerPublicKey}>Get</Button>
+                    </div>
+                    <div style={{
+                        minWidth: "40rem",
+                        marginBottom: "2rem",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between"
+                    }}>
+                        <Text>Public Key</Text>
+                        {serverPublicKey ?
+                            <Paragraph ellipsis={{rows: 2, expandable: true, symbol: 'more'}}>{serverPublicKey}</Paragraph>
+                            : null}
+                    </div>
+                </Card>
+                <Card title={'Messages'}>
+                    <div style={{
+                        minWidth: "40rem",
+                        marginBottom: "2rem",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between"
+                    }}>
+                        <Text>Encrypted message</Text>
+                        <Text></Text>
+                    </div>
+                    <div style={{
+                        minWidth: "40rem",
+                        marginBottom: "2rem",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between"
+                    }}>
+                        <Text>Decrypted message</Text>
+                        <Text></Text>
+                    </div>
+                </Card>
+            </div>
         </>
-  )
+    )
 }
---
+++ server/pages/api/generateKeys.js
@@ -0,0 +1,26 @@
+const crypto = require('crypto');
+const fs = require('fs');
+
+export default async (req, res) => {
+    const {publicKey, privateKey} = crypto.generateKeyPairSync('rsa', {
+        modulusLength: 4096,
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem',
+            cipher: 'aes-256-cbc',
+            passphrase: 'secret'
+        }
+    });
+    try{
+        fs.writeFileSync(`${__dirname}/keys.json`, JSON.stringify({publicKey, privateKey}), {flag:'w'}) ;
+
+    }
+    catch(e) {
+
+    }
+    res.status(200).json({publicKey, privateKey});
+}
